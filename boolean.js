4ce2bc1ea2ca1460ece1dc6ce64e38d6d8206ca4
Takeshi Kobayashi <60259718+Takeshi3727@users.noreply.github.com>
2020-04-21 16:26:54 +0900

    Create boolean.js
    boolean（正誤判定）の復習用として作成。

---
+++ boolean.js
@@ -0,0 +1,10 @@
+// 年齢の設定
+let age = 28;
+// Calculate is child - if they are 7 or under(7歳以下＝子供)
+let isChild = age <= 7;
+// Calculate is senior - if they are 65 or older(65歳以上 = シニア)
+let isSenior = age >= 65;
+// print is child value
+console.log(isChild);
+// print is senior value
+console.log(isSenior);
