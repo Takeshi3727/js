4506c4a5d5a0663d1eb82e9a4a114606f91ace2d
Takeshi Kobayashi <60259718+Takeshi3727@users.noreply.github.com>
2020-04-21 15:53:37 +0900

    Create temperture-conversion.js
    摂氏から華氏へ摂氏からカルビンへ変換できる簡単なJavaScriptアプリを作成しました！
    摂氏を設定することが出来れば、華氏やカルビンでの表示が可能に出来ます。
    日本での気温を華氏やカルビンで確認したい外国人向けを意識しました。
    letでの定義付けからのconsole.logでの出力の復習も兼ねました。

---
+++ temperture-conversion.js
@@ -0,0 +1,14 @@
+// 気温の定義付け
+let celsuis = 16;
+
+// 華氏(°F)への変換式
+let fahenhheit = celsuis * 9 / 5 + 32;
+
+// 華氏(°F)の表示
+console.log(fahenhheit);
+
+// カルビンへの変換式
+let kelvin = celsuis + 273.15;
+
+// print that value
+ console.log(kelvin);
