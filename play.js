ea5738a1b6a120ce94cf9a946bcf850a97f17efe
Takeshi Kobayashi <60259718+Takeshi3727@users.noreply.github.com>
2020-08-11 15:55:20 +0900

    Update play.js
    最初の方のコード（1-19行目）は、letとconstの定義付け。
    次のコードは、（20-行目）は、配列を意識しました。

--- play.js
+++ play.js
@@ -17,3 +17,16 @@ const addRandom = () => 1 + 2;
 
 console.log(addRandom());
 console.log(summerizeUser(name, age, hasHobbies));
+
+const person = {
+  name:'Max',
+  age: 29,
+  greet() {
+    console.log('Hi, I am ' + this.name);
+  }
+};
+
+const hobbies = ['Sports', 'Cooking'];
+
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+console.log(hobbies);
